d9657cac69489eed149bfeadf502489064aade92
IntrovertedSiko <IntrovertedSiko@gmail.com>
2014-06-04 08:38:39 -0700

    Update Clicker.js

--- Clicker.js
+++ Clicker.js
@@ -35,15 +35,15 @@
       
       var keys = [];                                                  // Konami Code stuff
       var konami = '38,38,40,40,37,39,37,39,66,65';
-    /*  $(document).keyDown(function(e){
+      $(document).keyDown(function(e){
         keys.push(e.keyCode);
-        if(keys.toString().indexOf(konami) >= 0){
+        if(keys.toString().indexOf(konami) > 0){
           clicks += 5000;
           //setTimeout(function(){autoClicker -= 5000}, 5000);
       
           keys = [];
         }
-      });*/
+      });
     }
     
     function unload(){                                              // Called when PSoD is clicked.
